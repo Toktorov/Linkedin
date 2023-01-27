d43c48d743b5aef6d9aca60ea60e32f3afd5aa38
Toktorov <ktoktorov2005@gmail.com>
2023-01-27 08:47:21 +0600

    add post models

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+from apps.users.models import User
+
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="users_post",
+        verbose_name="Пользователь"
+    )
+    title = models.CharField(
+        max_length=255,
+        verbose_name= "Заголовок"
+    )
+    image = models.ImageField(
+        upload_to="post_image/",
+        verbose_name="Фотография поста",
+        blank=True, null = True
+    )
+
+    def __str__(self):
+        return f"{self.user}"
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
