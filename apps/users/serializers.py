21ee05244431e3acc6db3497043244636bc20340
Toktorov <ktoktorov2005@gmail.com>
2023-01-27 08:32:41 +0600

    user detail endpoint

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -6,7 +6,16 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ('id', 'last_login', 'username',
+                'first_name', 'last_name', 'email',
+                'date_joined', 'profile_image', 'is_premium')
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = ('id', 'last_login', 'username',
+                'first_name', 'last_name', 'email',
+                'date_joined', 'profile_image', 'is_premium')
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,17 +1,20 @@
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from apps.users.models import User
-from apps.users.serializers import UserSerializer, UserRegisterSerializer
+from apps.users.serializers import UserSerializer, UserDetailSerializer, UserRegisterSerializer
 from apps.users.permissions import UsersPermissions
 
 # Create your views here.
-class UserAPIViewSet(GenericViewSet, ListModelMixin):
+class UserAPIViewSet(GenericViewSet, ListModelMixin,
+                        CreateModelMixin, RetrieveModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def get_serializer_class(self):
+        if self.action in ('retrieve', ):
+            return UserDetailSerializer
         if self.action in ('create', ):
             return UserRegisterSerializer
         return UserSerializer
