5678d599a1e2157bc09b4898e8d833e63eb13122
Toktorov <ktoktorov2005@gmail.com>
2023-01-30 06:51:33 +0600

    serializer validate

--- apps/chats/serializers.py
+++ apps/chats/serializers.py
@@ -8,7 +8,26 @@ class ChatSerializer(serializers.ModelSerializer):
         model = Chat 
         fields = "__all__"
 
+    def validate(self, attrs):
+        if attrs['from_user'] == attrs['to_user']:
+            raise serializers.ValidationError({"error": "Нельзя писать самому себе"})
+        if Chat.objects.filter(from_user = attrs['from_user'], to_user = attrs['to_user']).exists() or Chat.objects.filter(to_user = attrs['from_user'], from_user = attrs['to_user']).exists():
+            raise serializers.ValidationError({"exits" : "Такой чат уже существует"})
+        return 
+
 class ChatMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatMessage
         fields = "__all__"
+
+class ChatMessageCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatMessage
+        fields = "__all__"
+
+    def validate(self, attrs):
+        try:
+            chat = Chat.objects.get(pk = self.initial_data['chat'], from_chat_user = self.context['request'].user)
+        except:
+            chat = Chat.objects.get(pk = self.initial_data['chat'], to_chat_user = self.context['request'].user)
+        return attrs
