84767419f72b9b1095461f6df4e9d1ffb44d27d3
Toktorov <ktoktorov2005@gmail.com>
2023-01-31 06:40:23 +0600

    add model for likes, comments, images and favorites

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -25,3 +25,80 @@ class Post(models.Model):
     class Meta:
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
+
+class PostImages(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="post_images",
+        verbose_name="Пост"
+    )
+    image = models.ImageField(
+        upload_to="post_images/",
+        verbose_name="Фотография"
+    )
+    
+    def __str__(self):
+        return f"{self.id}, {self.post}"
+
+    class Meta:
+        verbose_name = "Фотография к посту"
+        verbose_name_plural = "Фотография к постам"
+
+class PostLike(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="post_likes",
+        verbose_name = "Пользователь"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="post_users",
+        verbose_name = "Пост"
+    )
+
+    def __str__(self):
+        return f"{self.post} - {self.user}"
+
+    class Meta:
+        verbose_name = "Лайки к посту"
+        verbose_name_plural = "Лайки к постам"
+        unique_together = ('user', 'post')
+
+class PostComment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="post_comment_user",
+        verbose_name = "Пользователь"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="post_commnets",
+        verbose_name = "Пост"
+    )
+
+    def __str__(self):
+        return f"{self.post} - {self.user}"
+
+    class Meta:
+        verbose_name = "Комментарий к посту"
+        verbose_name_plural = "Комментарии к постам"
+        unique_together = ('user', 'post')
+
+class PostFavorites(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="favorites",
+        verbose_name = "Пользователь"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="post_favotites",
+        verbose_name = "Пост"
+    )
+    def __str__(self):
+        return f"{self.post} - {self.user}"
+
+    class Meta:
+        verbose_name = "Избранное"
+        verbose_name_plural = "Избранные"
+        unique_together = ('user', 'post')
