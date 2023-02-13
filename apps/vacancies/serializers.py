2b8a5b852564f6afff774a4765a265cc122a24bc
Toktorov <ktoktorov2005@gmail.com>
2023-02-13 15:47:30 +0600

    vacancy organization endpoints

--- apps/vacancies/serializers.py
+++ apps/vacancies/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 
-from apps.vacancies.models import Vacancy
+from apps.vacancies.models import Vacancy, VacancyFavorite, OrganizationVacancy, OrganizationVacancyFavorite
 
 
 class VacancySerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacancy
         fields = ('id', 'user', 'title', 'description', 'created')
+
+class VacancyFavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VacancyFavorite
+        fields = ('id', 'user', 'vacancy')
+
+class OrganizationVacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationVacancy
+        fields = ('id', 'organization', 'user', 'title', 'description', 'created')
+
+class OrganizationVacancyFavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationVacancyFavorite
+        fields = ('id', 'organization', 'user', 'vacancy')
--- apps/vacancies/urls.py
+++ apps/vacancies/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path 
 from rest_framework.routers import DefaultRouter
 
-from apps.vacancies.views import VacancyAPIViewSet
+from apps.vacancies.views import VacancyAPIViewSet, VacancyFavoriteAPIViewSet, OrganizationVacancyAPIViewSet, OrganizationVacancyFavoriteAPIViewSet
 
 router = DefaultRouter()
 router.register(prefix='vacancy', viewset=VacancyAPIViewSet)
+router.register(prefix='vacancy_favorite', viewset=VacancyFavoriteAPIViewSet)
+router.register(prefix='organization_vacancy', viewset=OrganizationVacancyAPIViewSet)
+router.register(prefix='organization_vacancy_favorite', viewset=OrganizationVacancyFavoriteAPIViewSet)
 
 urlpatterns = router.urls
